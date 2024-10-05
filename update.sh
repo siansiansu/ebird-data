0182fe1fba7179168f952e68555fd8b447069c44
Alex Su <alex.su@btse.com>
2024-10-06 03:52:37 +0800

    update

--- update.sh
+++ update.sh
@@ -1,20 +1,26 @@
 source ./EBIRD_API_KEY
 
 ## CSV
-# Update Country Data
+
+echo "Update Country Data..."
+
 curl -s --location 'https://api.ebird.org/v2/ref/region/list/country/world?fmt=csv' \
      --header "x-ebirdapitoken: ${EBIRD_API_KEY}" -o ./csv/country.csv
 
-# Update TW Region Data
-curl -s --location "https://api.ebird.org/v2/ref/hotspot/TW?fmt=csv" \
-     --header "x-ebirdapitoken: ${EBIRD_API_KEY}" -o taiwan_region.csv
+echo "Update Taiwan Region Data..."
 
-# Update Subnational1 Data
+curl -s --location "https://api.ebird.org/v2/ref/hotspot/TW?fmt=csv" \
+     --header "x-ebirdapitoken: ${EBIRD_API_KEY}" -o ./csv/taiwan_region.csv
+
+echo "Update Subnational1 Data..."
+
 curl -s --location 'https://api.ebird.org/v2/ref/region/list/subnational1/TW?fmt=csv' \
      --header "x-ebirdapitoken: ${EBIRD_API_KEY}" -o ./csv/subnational1.csv
 
 ## JSON
-# Update Region Data
+
+echo "Update Region Data..."
+
 regionList="TW JP"
 combined=$(for region in $regionList
 do
@@ -26,7 +32,8 @@ done | jq -s 'add | map(del(.[] | select(. == null)))')
 
 echo "$combined" | sed $'s/\t/ /g' | sed 's/\x08/\\u0008/g'  > ./json/region.json
 
-# Update Species Data
+echo "Update Species Data..."
+
 curl -s --location 'https://api.ebird.org/v2/ref/taxonomy/ebird?fmt=json&locale=en' -o ./json/species_en.json
 curl -s --location 'https://api.ebird.org/v2/ref/taxonomy/ebird?fmt=json&locale=ja' -o ./json/species_ja.json
 curl -s --location 'https://api.ebird.org/v2/ref/taxonomy/ebird?fmt=json&locale=zh' -o ./json/species_zh.json
